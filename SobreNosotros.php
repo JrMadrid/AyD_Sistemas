f328f4e3730a7345bd914d7a506559d108cba621
Juan Madrid <72727166+JrMadrid@users.noreply.github.com>
2022-03-15 15:55:26 -0600

    preparando sobrenosotros

--- SobreNosotros.php
+++ SobreNosotros.php
@@ -1,10 +1,6 @@
 <?php
 include_once("elementos/encabezado.php";)
 ?>
-Esta es la pagina sobre la empresa
-
-modifica esto juan :D 
-llevas meses olvidandolo
 
 <?php
 include_once("elementos/piepagina.php";)
